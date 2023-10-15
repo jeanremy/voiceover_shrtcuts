f8b697f4661a26b252a11d75192ae055e7468faf
JR <contact@jeanremypraud.com>
2023-10-15 22:21:29 +0200

    :rocket: Add build action

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,4 +5,6 @@ import preact from '@astrojs/preact'
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact({ compat: true })],
+  site: 'https://jeanremy.github.io',
+  base: '/voiceover-shrtcts',
 })
